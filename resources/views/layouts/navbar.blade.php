ba30f98bb1293d5c7230717ed4f03af3454c4416
Eduarda Brisch Dalmás <du.dalmas@hotmail.com>
2024-06-25 11:59:51 -0300

    reposicionando dropdown

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -14,32 +14,33 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('home') }}">{{ __('Questionários') }}</a>
                 </li>
-
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }}
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('home') }}" onclick="event.preventDefault(); document.getElementById('home-form').submit();">
-                            {{ __('Home') }}
-                        </a>
-
-                        <form id="home-form" action="{{ route('home') }}" method="GET" class="d-none">
-                            @csrf
-                        </form>
-                        
-                        <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
             </ul>
+        </div>
+        <div class="d-flex">
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }}
+                </a>
+    
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('home') }}" onclick="event.preventDefault(); document.getElementById('home-form').submit();">
+                        {{ __('Home') }}
+                    </a>
+    
+                    <form id="home-form" action="{{ route('home') }}" method="GET" class="d-none">
+                        @csrf
+                    </form>
+                    
+                    <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+    
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
         </div>
     </div>
 </nav>
