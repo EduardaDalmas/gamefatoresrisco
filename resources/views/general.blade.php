3fa8b6f69183613c91a8214acba70cb8e8596150
Eduarda Brisch Dalmás <du.dalmas@hotmail.com>
2024-06-25 20:13:09 -0300

    melhoria visdualização opções

--- resources/views/general.blade.php
+++ resources/views/general.blade.php
@@ -29,6 +29,8 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.2/font/bootstrap-icons.min.css">
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
 
         <script src="{{ asset('js/ajax.js') }}" defer></script>
--- resources/views/question/view.blade.php
+++ resources/views/question/view.blade.php
@@ -6,9 +6,17 @@
             <div class="col-md-8 mt-5 mb-5">
                 <div class="card centralizar p-5">
                     <div class="card-body">
-                        <a href="{{ route('topic.edit', ['topic' => $question->topic]) }}">Voltar</a>
+                        <div class="mb-3 row align-items-center">
+                            <div class="col-auto">
+                                <a href="{{ route('topic.edit', ['topic' => $question->topic]) }}"> <i class="bi bi-arrow-left-short icone"></i></a>
+                            </div>
+                            <div class="col-auto">
+                                <h4>Opções de resposta</h4>
+                            </div>
+                        </div>
+                        
                         <div class="mb-3 centralizar">
-                            <h4>{{ $question->description }}</h4>
+                            <h5 style="color: black">{{ $question->description }}</h5>
                         </div>
 
                         <form method="POST" action="{{ route('option.save', ['question' => $question]) }}">
@@ -17,38 +25,55 @@
                                 <label for="resposta">Adicionar opção de resposta</label>
                                 <textarea class="form-control" name="resposta" id="resposta" style="width: 100%" rows="4"></textarea>
                             </div>
-                            <div class="form-group col-12">
-                                <button type="submit" class="btn btn-primary">Salvar</button>
+                            <div class="form-group centralizar">
+                                <button type="submit" class="btn btn-custom">Salvar</button>
                             </div>
                         </form>
                         <br><hr><br>
-                        @foreach($options as $option)
-                            <div class="col-12">
-                                <div class="row col-8">
-                                    <h4>{{ $option->name }}</h4>
+                        <div class="mb-3 centralizar">
+                            <h5 style="color: black">Alternativas</h5>
+                        </div>
+
+                    
+                        <div class="container mt-5">
+                            <!-- Cabeçalhos das colunas -->
+                            <div class="row mb-3">
+                                <div class="col-8">
+                                    <strong>Descrição</strong>
                                 </div>
-                                <div class="row">
-                                    <div class="col-8">
-                                        <p>{{ $option->description }}</p>
-                                    </div>
-                                    <div class="col-2 form-group">
-                                        <label for="ratio">Peso</label>
-                                        <input class="form-control" type="number" id="ratio" value="{{ $option->ratio }}" {{ $option->trashed() ? 'disabled' : '' }}>
-                                    </div>
-                                    @if($option->trashed())
-                                        <div class="col-2 form-group">
-                                            <label> - </label>
-                                            <a href="{{ route('option.restore', ['option' => $option]) }}" class="btn btn-primary">Reativar</a>
-                                        </div>
-                                    @else
-                                        <div class="col-2 form-group">
-                                            <label> - </label>
-                                            <a href="{{ route('option.delete', ['option' => $option]) }}" class="btn btn-danger">Excluir</a>
-                                        </div>
-                                    @endif
+                                <div class="col-2">
+                                    <strong>Peso</strong>
+                                </div>
+                                <div class="col-2">
+                                    <strong>Opções</strong>
                                 </div>
                             </div>
-                        @endforeach
+                        
+                            <!-- Loop através das opções -->
+                            @foreach ($options as $index => $option)
+                                <div class="row mb-3">
+                                    <!-- Radio button e label -->
+                                    <div class="col-8">
+                                        <input type="radio" class="btn-check" name="vbtn-radio" id="vbtn-radio{{ $index }}" autocomplete="off" value="{{ $option->id }}">
+                                        <label class="btn btn-outline-info mb-0 ms-2" for="vbtn-radio{{ $index }}">{{ $option->name }} - {{ $option->description }}</label>
+                                    </div>
+                                    
+                                    <!-- Input de Peso -->
+                                    <div class="col-2 form-group">
+                                        <input class="form-control" type="number" id="ratio{{ $index }}" value="{{ $option->ratio }}" {{ $option->trashed() ? 'disabled' : '' }}>
+                                    </div>
+                                    
+                                    <!-- Botão Restaurar ou Excluir -->
+                                    <div class="col-2 form-group">
+                                        @if ($option->trashed())
+                                            <a href="{{ route('option.restore', ['option' => $option]) }}" class="btn btn-primary">Reativar</a>
+                                        @else
+                                            <a href="{{ route('option.delete', ['option' => $option]) }}" class="btn btn-danger">Excluir</a>
+                                        @endif
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
