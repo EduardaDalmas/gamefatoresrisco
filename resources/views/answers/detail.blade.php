0eafc963176844206f1f6ec350208d7146d2efa1
Eduarda Brisch Dalmás <du.dalmas@hotmail.com>
2024-06-28 10:26:57 -0300

    ajustes na apresentação de respostas

--- resources/views/answers/detail.blade.php
+++ resources/views/answers/detail.blade.php
@@ -14,34 +14,42 @@
                                 <h1 class="h1 text-dark">Respostas: {{ $data['person']->name }}</h1>
                             </div>
                         </div>
+                        <hr>
 
                         <div class="container">
-                            <div class="col-auto">
+                            {{-- <div class="col-auto">
                                 <h4>Tópicos</h4>
                             </div>
-                            <hr>
+                            <hr> --}}
                             @foreach ($data['estructure_questionnaire'] as $topic)
-                                <p>
-                                    <button class="btn btn-custom" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-topic-{{ $loop->index }}" aria-expanded="false" aria-controls="collapse-topic-{{ $loop->index }}">
-                                        <span class="icon-expand bi bi-chevron-down"></span>
-                                        <span class="icon-collapse bi bi-chevron-up" style="display: none;"></span>
-                                        {{ $topic->name }}
-                                    </button>
-                                </p>
-                                <div>
-                                    <div class="collapse collapse-horizontal" id="collapse-topic-{{ $loop->index }}">
+                               
+                                {{-- <button class="btn btn-custom" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-topic-{{ $loop->index }}" aria-expanded="false" aria-controls="collapse-topic-{{ $loop->index }}">
+                                    <span class="icon-expand bi bi-chevron-down"></span>
+                                    <span class="icon-collapse bi bi-chevron-up" style="display: none;"></span>
+                                    {{ $topic->name }}
+                                </button> --}}
+                                <div class="mb-5">
+                                    <div class="row">
+                                        <div class="col-auto"><i class="bi bi-bookmark-check icone-topico"></i></div>
+                                        <div class="col-auto"><h4>Tópico:  {{ $topic->name }}</h4></div>
+                                        
+                                        
+                                    </div>
+                                
+                                    {{-- <div class="collapse collapse-horizontal" id="collapse-topic-{{ $loop->index }}"> --}}
                                         <div class="card card-body">
-                                            <div class="col-auto">
-                                                <h4>Questões</h4>
-                                            </div>
                                             @foreach ($topic->questions as $question)
                                                 <p>
-                                                   <b> Questão:  {{ $question->description }} </b>
+                                                    <b> Questão:  {{ $question->description }} </b>
                                                 </p>
                                                 <div class="col-auto">
                                                     <h6>Alternativas</h6>
                                                 </div>
-                                                @foreach ($question->options as $option)
+                                                @foreach ($question->options as $index => $option)
+
+                                                @php
+                                                    $optionsCount = count($question->options);
+                                                @endphp
                                                     <div class="option {{ $option->person_answer ? 'selected-answer' : '' }}">
                                                         @if ($option->person_answer)
                                                             <i class="bi bi-check-circle-fill" style="color: green;"></i> {{ $option->description }}
@@ -49,11 +57,13 @@
                                                             {{ $option->description }}
                                                         @endif
                                                     </div>
+                                                    @if ($index < $optionsCount - 1)
+                                                        <hr>
+                                                    @endif
                                                 @endforeach
-                                                <hr>
                                             @endforeach
                                         </div>
-                                    </div>
+                                    {{-- </div> --}}
                                 </div>
                             @endforeach
                         </div>
